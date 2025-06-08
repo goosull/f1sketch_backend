0a110ac75c6101643cd03c28566aa8b1cc6b4730
goosull <134674369+goosull@users.noreply.github.com>
2025-06-08 17:43:55 +0900

    feat: enhance Swagger setup with custom UI options

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,20 @@ async function bootstrap() {
     .setDescription('The is a sample REST API')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'My API Docs',
+    customfavIcon: 'https://example.com/favicon.ico',
+    // Swagger UI 번들 JS 파일들
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    // Swagger UI CSS 파일들
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
